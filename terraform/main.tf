13e3c676b3c302d22ee97e201bae0ddcff372536
glamprinakis <73303072+glamprinakis@users.noreply.github.com>
2025-07-30 23:47:28 +0300

    added SSH access to the security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -128,6 +128,15 @@ resource "aws_security_group" "web" {
     description = "HTTPS"
   }
 
+  # SSH
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
   # SSH (restrict to your IP for security)
   ingress {
     from_port   = 22
